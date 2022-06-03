339890153a9f285e4e990c8e62ebd5905b627121
ItsFeny <fenymartinez459@gmail.com>
2022-06-03 01:20:16 -0400

    UI, Funcion y LocalStorage

--- App.js
+++ App.js
@@ -1,35 +1,254 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 
 
-//Estilos
+//Estilos importaciones
 import 
 {
    StyleSheet,
+   SafeAreaView,
+   View,
    Text,
-   View 
+   TextInput,
+   TouchableOpacity,
+   FlatList,
+   Alert  
 } 
 from 'react-native';
 
 
+//Importacion de los iconos
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//Importacion del AsynStorage almacenamiento local
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
 
+//Cuerpo de la pagina
+const App = () =>  {
+
+
+  const [tasks, setTask] = React.useState([
+    {id:1, task: 'Primera Tarea ', completed: false},
+    {id:2, task: 'Segunda Tarea ', completed: false},
+  ]);
+
+  
+  const [textInput, setTextInput] = React.useState('');
+
+
+  React.useEffect(() => {
+    getTaskFromUserDevice();
+  }, []);
+
+  React.useEffect(() => {
+    saveTaskToUserDevice(tasks);
+  }, [tasks]);
+
+
+  const saveTaskToUserDevice = async tasks => {
+    try {
+      const stringifytasks = JSON.stringify(tasks);
+      await AsyncStorage.setItem('tasks', stringifytasks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTaskFromUserDevice = async () => {
+    try {
+      const tasks = await AsyncStorage.getItem('tasks');
+      if (tasks != null) {
+        setTask(JSON.parse(tasks));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  const ListItem = ({task}) => {
+    return <View style={styles.listTask}> 
+             <View style={{flex:1}}>
+             <Text style={{color:'white', textDecorationLine: task?.completed ? 'line-through': 'none'}}>
+               {task?.task}
+            </Text>
+            </View>
+            {
+              !task?.completed && (
+              <TouchableOpacity style={styles.actionButton} onPress={() => markTaskComplete(task.id)}>
+                <Icon name="done" style={{color:'white'}} size={20} />
+             </TouchableOpacity>
+              )
+            }
+           
+            <TouchableOpacity style={styles.actionButton} onPress={() => deleteTask(task.id)}>
+               <Icon name="delete" style={{color:'white'}} size={20} />
+            </TouchableOpacity>
+       </View>
+  };
+
+
+
+  const AddTask = () => {
+     
+     if(textInput == '')
+     {
+       Alert.alert("Debes Ingresar Una Tarea");
+     }
+     else
+     {
+      const newTask =
+      {
+        id: Math.random(),
+        task: textInput,
+        completed: false
+      };
+        setTask([...tasks,newTask]);
+        setTextInput('');
+     }   
+};
+
+
+ 
+const markTaskComplete = taskId => {
+  const newTaskItem = tasks.map(item => {
+    if (item.id == taskId) {
+      return {...item, completed: true};
+    }
+    return item;
+  });
+
+  setTask(newTaskItem);
+};
+
+
+const deleteTask = taskId => {
+  const newTaskItem = tasks.filter(item => item.id != taskId);
+  setTask(newTaskItem);
+};
+
+
+  return <SafeAreaView style={styles.container}>
+    
+
+     <View style={styles.header}>
+         <Text style={styles.title}>TO-DO</Text> 
+     </View>
+
+   
+     <FlatList 
+     data={tasks} 
+     renderItem={({item}) => <ListItem task={item}/> }
+     showsVerticalScrollIndicator={false}
+     contentContainerStyle={{padding: 20, paddingBottom: 100}} 
+     />
+
+
+      <View style={styles.footer}>
+        <View style={styles.footer_input}>
+              <TextInput placeholder="Add New Task" 
+                         onChangeText={(text) => setTextInput(text)}
+                         value={textInput}/>
+           </View>
+         <TouchableOpacity onPress={AddTask}> 
+            <View style={styles.addTask}>
+              <Icon name="add" size={30} style={styles.iconButtonTask} />
+            </View>
+         </TouchableOpacity>
+      </View>
+      
+</SafeAreaView>;
+
+};
+
+
+
+
+
+
+
+//Configuracion de los estilos
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+     
+     container: 
+     {
+       flex:1,
+       backgroundColor: 'white',
+     },
+     header: 
+     {
+       padding:20,
+       flexDirection:'row',
+       alignItems: 'center',
+       justifyContent:'center'
+     },
+     title: 
+     {
+       fontSize:30,
+       marginTop:20,
+       
+     },
+     footer: 
+     {
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+       
+     },
+     footer_input: 
+     {
+      height: 50,
+      paddingHorizontal: 20,
+      elevation: 40,
+      flex: 1,
+      marginVertical: 20,
+      marginRight: 20,
+      borderRadius: 30,
+      borderWidth:1,
+      borderColor:'blue',
+      justifyContent:'center',
+      backgroundColor: 'white',
+     },
+     addTask:
+     {
+      height: 50,
+      width: 50,
+      backgroundColor: 'blue',
+      elevation: 40,
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+     },
+     iconButtonTask:
+     {
+      color:'white',
+     },
+     listTask:
+     {
+      padding: 20,
+      backgroundColor: 'lightslategrey',
+      flexDirection: 'row',
+      elevation: 12,
+      borderRadius: 7,
+      marginVertical: 10,
+     },
+     actionButton:
+     {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 20,
+     },
+     
 });
+
+
+
+
+
+export default App;
